cfe95085d58fa021f9504b7e0f3bf1c79a70a412
Zhicong Zang <zzc19920809@yahoo.com>
2016-08-02 19:23:41 -0500

    no message

---
+++ DailyNotes/DetailPhotosCollectionLayout.swift
@@ -0,0 +1,13 @@
+//
+//  DetailPhotosCollectionLayout.swift
+//  DailyNotes
+//
+//  Created by Zhicong Zang on 8/2/16.
+//  Copyright © 2016 Zhicong Zang. All rights reserved.
+//
+
+import UIKit
+
+class DetailPhotosCollectionLayout: UICollectionViewFlowLayout {
+
+}
---
+++ DailyNotes/DetailPhotosViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailPhotosViewController.swift
+//  DailyNotes
+//
+//  Created by Zhicong Zang on 8/2/16.
+//  Copyright © 2016 Zhicong Zang. All rights reserved.
+//
+
+import UIKit
+
+class DetailPhotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
